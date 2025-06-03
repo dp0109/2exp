46d5a38a7cd31707137a938ea57010f3b2985140
dp0109 <diyabhadani01@gmail.com>
2025-06-03 21:46:11 +0530

    Add RectanglePerimeter class

---
+++ RectanglePerimeter.java
@@ -0,0 +1,21 @@
+//package 2exp;
+
+// File: RectanglePerimeter.java
+public class RectanglePerimeter {
+    private double length;
+    private double breadth;
+
+    public RectanglePerimeter(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + breadth);
+    }
+
+    public static void main(String[] args) {
+        RectanglePerimeter rectangle = new RectanglePerimeter(10, 5);
+        System.out.println("Perimeter of Rectangle: " + rectangle.calculatePerimeter());
+    }
+}
